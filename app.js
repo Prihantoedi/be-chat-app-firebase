4655682c26c7928ab19fef0dea76cb319bb151f1
Prihantoedi <prihanto_sanjaya@yahoo.com>
2023-09-02 08:19:21 +0700

    installing some package in node module: - cors: npm install cors - body-parser: npm install body-parser

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { firebaseConfig } from "./FirebaseConfig.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import express from "express";
 
 dotenv.config();
 
-// const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
+let allowedOrigins = ["http://localhost"];
+app.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            let msg = "The CORS policy for this site does not allow access from the specified Origin. ";
+            return callback(new Error(msg), false);
+        }
+
+        return callback(null, true);
+    }
+    // origin: "http://localhost:80"
+}));
+
 const PORT = process.env.PORT || 3000;
-// const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
@@ -21,10 +36,15 @@ app.listen(PORT, () => {
 
 
 app.get("/status", (request, response) => {
-    const status = {
+    const res = {
         "Status" : "Running",
         "FirebaseConfig": firebaseConfig,
     };
 
-    response.send(status);
+    response.status(200).send(res);
+});
+
+app.get("/api/chat-app-firebase/v1/signin", (request, response) => {
+    console.log(request.body);
+    response.status(200).send("It's work!");
 });
